be452aea17144b2c57ebf01a6ce27ea36fec0381
DeeptiTyagii <tyagideepti4@gmail.com>
2025-06-04 18:27:28 +0530

    Added Authorisation - Auth Controller and Register action method

---
+++ NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Domain.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    //this is to create a new user!
+
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // POST : /api/Auth/Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+
+            var identityResult = await _userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if (identityResult.Succeeded)
+            {
+                //Add roles for this user
+
+                if(registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await _userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+
+                    if(identityResult.Succeeded)
+                    {
+                        return Ok("User was registered successfully! Please Login.");
+                    }
+                }
+                
+                
+            }
+
+            return BadRequest("Something went wrong.");
+
+        }
+    }
+}
